31583d277304c3a614c41e50f6c025d8b5e37ef6
Rahul917-Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2025-02-07 22:42:08 +0530

    api to fetch users based on search

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from .views import UserCreateView
+from .views import UserCreateView, SearchUserView
 
 urlpatterns = [
-    path('user/', UserCreateView.as_view(), name='search_user'),
+    path('user/', UserCreateView.as_view(), name='store_get_user'),
+    path('search-user/', SearchUserView.as_view(), name='search_user'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -18,4 +18,34 @@ class UserCreateView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-    
+
+class SearchUserView(APIView):
+    def post(self, request):
+        try:
+            query = request.data["search"]
+            if len(query) == 0:
+                return Response({"error": "Name parameter is required"}, status=400)
+            
+            users = User.objects.filter(first_name__istartswith=query)
+            serializer = UserSerializer(users, many=True)
+            user_list = serializer.data
+            
+            message = "successfully fetched users list"
+            statusCode = status.HTTP_200_OK
+
+            if len(user_list) == 0:
+                message = "No user found with the given search"
+                statusCode = status.HTTP_400_BAD_REQUEST
+            return Response({
+                "data":user_list,
+                "message":message
+            },
+            status=statusCode
+            )
+        except:
+            return Response({
+                "data":user_list,
+                "message":message
+            },
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
