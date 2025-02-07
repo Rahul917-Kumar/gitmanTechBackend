f0673b8b5543fa17dec89282c03a3dd4626ed03a
Rahul917-Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2025-02-07 20:14:32 +0530

    add model in users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    city = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=15)
+    profile_pic = models.TextField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
