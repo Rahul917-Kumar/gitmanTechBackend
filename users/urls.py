a5d7dda790e11b2df71db6ade3c822495ce13d1d
Rahul917-Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2025-02-07 22:13:17 +0530

    create endpoint to get and store user

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+from .views import UserCreateView
+
+urlpatterns = [
+    path('user/', UserCreateView.as_view(), name='search_user'),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserCreateView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data['data'], many=True)
+        if serializer.is_valid():
+            serializer.save()  # Saves user data to DB
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        users = User.objects.all()  # Retrieve all users
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    
